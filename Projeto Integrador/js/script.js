74b3aaaef2da22529169bc1ca964a0cf302f540d
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-03-23 19:33:51 -0300

    transferencia do pacote do site

---
+++ Projeto Integrador/js/script.js
@@ -0,0 +1,27 @@
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// }
+window.scrollTo(0, 0);
+// function scrollToFirstText(formulario){
+//     var valor_busca = formulario.querySelector('input').value;
+//     alert(valor_busca);
+//     var secoesList = document.body.querySelectorAll('main section');
+//     alert(secoesList);
+//     alert(secoesList[0].textContent);
+//     for(let cont = 0; cont< secoesList; cont++){
+//         var lista=[];
+//         let cont2 = 0;
+//         let trimSection = sections[0].textContent.trim().split("\n");
+//         for(let cont = 0;cont<trimSection.length;cont++){
+//             let valor = trimSection[cont].trim();
+//             if(valor!=""){
+//                 lista[cont2]=valor; cont2++;
+//             }
+//         }
+//         if (lista[cont].textContent.(valor_busca)){
+//             alert('FOUND IT!!!!! NA SECTION: '+(cont+1));
+//         } else {
+//             alert('NADA NA SEÇÃO: '+(cont+1));
+//         }
+//     }
+// }
