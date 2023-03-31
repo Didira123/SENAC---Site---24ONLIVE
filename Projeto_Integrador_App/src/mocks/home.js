07711ae1353f949ce12d4e5c688de047fd48a16c
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-03-30 21:50:27 -0300

    versao App inicio

---
+++ Projeto_Integrador_App/src/mocks/home.js
@@ -0,0 +1,21 @@
+import logoHome from "../images/Home Page/LogoHome.png";
+
+export {
+    textos, css, imagens, htmls
+}
+
+const textos = {
+
+}
+
+const css = {
+    
+}
+
+const imagens = {
+    logoHome,
+}
+
+const htmls = {
+    
+}
