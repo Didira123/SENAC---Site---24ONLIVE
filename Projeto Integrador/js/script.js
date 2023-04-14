621e833999c0048b306af28d87c75ab9b5914337
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-04-13 21:53:00 -0300

    mais partes do carrinho

--- Projeto Integrador/js/script.js
+++ Projeto Integrador/js/script.js
@@ -1,39 +1,12 @@
-// window.onbeforeunload = function () {
-//     window.scrollTo(0, 0);
-// }
-// function scrollToFirstText(formulario){
-//     var valor_busca = formulario.querySelector('input').value;
-//     alert(valor_busca);
-//     var secoesList = document.body.querySelectorAll('main section');
-//     alert(secoesList);
-//     alert(secoesList[0].textContent);
-//     for(let cont = 0; cont< secoesList; cont++){
-//         var lista=[];
-//         let cont2 = 0;
-//         let trimSection = sections[0].textContent.trim().split("\n");
-//         for(let cont = 0;cont<trimSection.length;cont++){
-//             let valor = trimSection[cont].trim();
-//             if(valor!=""){
-//                 lista[cont2]=valor; cont2++;
-//             }
-//         }
-//         if (lista[cont].textContent.(valor_busca)){
-//             alert('FOUND IT!!!!! NA SECTION: '+(cont+1));
-//         } else {
-//             alert('NADA NA SEÇÃO: '+(cont+1));
-//         }
-//     }
-// }
-
 window.scrollTo(0, 0);
-function getOne(objeto){
+function $(objeto){
     // alert("AAAAAAA");
     return document.querySelector(objeto);
 }
-function getMany(objeto){
+function $$(objeto){
     return document.querySelectorAll(objeto);
 }
 
-getOne(".carrinho").addEventListener('click', function(){
-    getOne(".carrinho_invisivel").classList.add("carrinho_move");
+$(".carrinho_button").addEventListener('click', function(){
+    $(".carrinho_invisivel").classList.add("carrinho_move");
 })
