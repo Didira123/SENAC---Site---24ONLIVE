4c0ffb8d196000bdc3414795ff55ca14a5342f8e
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-04-17 21:51:00 -0300

    carrinho feito para PC

--- Projeto Integrador/js/script.js
+++ Projeto Integrador/js/script.js
@@ -10,3 +10,6 @@ function $$(objeto){
 $(".carrinho_button").addEventListener('click', function(){
     $(".carrinho_invisivel").classList.add("carrinho_move");
 })
+$(".topo_carrinho img").addEventListener('click', function(){
+    $(".carrinho_invisivel").classList.remove("carrinho_move");
+})
