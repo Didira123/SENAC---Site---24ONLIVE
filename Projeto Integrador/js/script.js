4676f131755760c821cd7d47cec3b2076630e068
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-04-19 21:47:36 -0300

    carrinho Mais e Menos

--- Projeto Integrador/js/script.js
+++ Projeto Integrador/js/script.js
@@ -7,9 +7,18 @@ function $$(objeto){
     return document.querySelectorAll(objeto);
 }
 
-$(".carrinho_button").addEventListener('click', function(){
+$(".carrinho_button").addEventListener('click', function(event){
     $(".carrinho_invisivel").classList.add("carrinho_move");
 })
-$(".topo_carrinho img").addEventListener('click', function(){
+$(".topo_carrinho img").addEventListener('click', function(event){
     $(".carrinho_invisivel").classList.remove("carrinho_move");
 })
+
+function maisUm(event){
+    let valor = event.target.parentNode.parentNode.querySelector(".quantiaProduto > p");
+    valor.textContent = parseInt(valor.textContent) + 1;
+}
+function menosUm(event){
+    let valor = event.target.parentNode.parentNode.querySelector(".quantiaProduto > p");
+    valor.textContent = parseInt(valor.textContent) - 1;
+}
